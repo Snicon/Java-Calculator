68cf8e571463607e62f79623e4b7807596729a04
Sixten Peterson <sixten.peterson@winterzone.se>
2023-02-13 08:41:37 +0100

    Support for minimizing and closing the app. Started work on label.

--- src/main/MainWindowController.java
+++ src/main/MainWindowController.java
@@ -1,11 +1,15 @@
 package main;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainWindowController {
     @FXML private Pane titlePane;
+    @FXML private ImageView btnMinimize, btnClose;
+    @FXML private Label lblResult;
 
     private double x, y;
 
@@ -18,5 +22,9 @@ public class MainWindowController {
             stage.setX(mouseEvent.getScreenX()-x);
             stage.setY(mouseEvent.getScreenY()-y);
         });
+
+        btnClose.setOnMouseClicked(mouseEvent -> stage.close());
+        // setIconified => Hide/minimize the application.
+        btnMinimize.setOnMouseClicked(mouseEvent -> stage.setIconified(true));
     }
 }
